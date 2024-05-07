34179c259a53f01bdc3f84cc8df091d6feee8cea
Heimbrand <olle.heimbrand@hotmail.se>
2024-05-07 13:15:16 +0200

    Added InventoryItem collection, Setup config in InventoryRepository, Added Dbsets in DnDbContext

--- DungeonsAndDragons-ToolAndBuilder.Mongo/Repositories/InventoryRepository.cs
+++ DungeonsAndDragons-ToolAndBuilder.Mongo/Repositories/InventoryRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace DungeonsAndDragons_ToolAndBuilder.Mongo.Repositories;
+﻿using DungeonsAndDragons_ToolAndBuilder.Shared.Collections;
+using MongoDB.Driver;
+
+namespace DungeonsAndDragons_ToolAndBuilder.Mongo.Repositories;
 
 public class InventoryRepository
 {
+    private readonly IMongoCollection<Inventory> _inventory;
+    public InventoryRepository()
+    {
+        var hostName = "localhost";
+        var port = "27017";
+        var dataBaseName = "DungeonsAndDragonsToolAndBuilder";
+        var collectionName = "Inventory";
+        var client = new MongoClient($"mongoDb://{hostName}:{port}");
+        var database = client.GetDatabase(dataBaseName);
+        _inventory = database.GetCollection<Inventory>(collectionName, new MongoCollectionSettings(){AssignIdOnInsert = true});
+    }
     
 }
--- DungeonsAndDragons-ToolAndBuilder.SQL/DnDbContext.cs
+++ DungeonsAndDragons-ToolAndBuilder.SQL/DnDbContext.cs
@@ -13,10 +13,12 @@ public class DnDbContext : DbContext
     public DbSet<Crafting> Crafting { get; set; }
     public DbSet<Event> Events { get; set; }
     public DbSet<Faction> Factions { get; set; }
+    public DbSet<GameAction> GameActions { get; set; }
     public DbSet<Gear> Gear { get; set; }
     public DbSet<Heirloom> Heirlooms { get; set; }
     public DbSet<Location> Locations { get; set; }
     public DbSet<MagicalArtifact> MagicalArtifacts { get; set; }
+    public DbSet<MiscellaneousItem> MiscellaneousItems { get; set; }
     public DbSet<Monster> Monsters { get; set; }
     public DbSet<Npc> Npcs { get; set; }
     public DbSet<QuestItem> QuestItems { get; set; }
@@ -24,11 +26,7 @@ public class DnDbContext : DbContext
     public DbSet<SubClass> SubClasses { get; set; }
     public DbSet<SubRace> SubRaces { get; set; }
     public DbSet<Weapon> Weapons { get; set; }
-    public DbSet<MiscellaneousItem> MiscellaneousItems { get; set; }
     public DbSet<World> Worlds { get; set; }
-    public DbSet<GameAction> GameActions { get; set; }
-
-
 
     public DnDbContext(DbContextOptions<DnDbContext> options) : base(options)
     {
