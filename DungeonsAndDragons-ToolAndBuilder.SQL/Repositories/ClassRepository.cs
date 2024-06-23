5567a7ae8ad903d932e6a7353db05b938da1db0e
Heimbrand <olle.heimbrand@hotmail.se>
2024-06-23 17:08:25 +0200

    Wrote methods in the classrepository

--- DungeonsAndDragons-ToolAndBuilder.SQL/Repositories/ClassRepository.cs
+++ DungeonsAndDragons-ToolAndBuilder.SQL/Repositories/ClassRepository.cs
@@ -12,8 +12,9 @@ public class ClassRepository(DnDbContext context) : IClassRepository
 
         if (classById is null)
             throw new Exception("No Class found with that ID");
-    }
 
+        return classById;
+    }
     public async Task<IEnumerable<Class>> GetAllAsync()
     {
         var allClasses = await context.Classes.ToListAsync();
@@ -23,34 +24,57 @@ public class ClassRepository(DnDbContext context) : IClassRepository
 
         return allClasses;
     }
-
     public async Task<IEnumerable<Class>> GetMany(int start, int count)
     {
-        throw new NotImplementedException();
-    }
+        var getMany = context.Classes.Skip(start).Take(count);
 
+        if (getMany is null)
+            throw new Exception("No classes found");
+
+        return getMany;
+    }
     public async Task AddAsync(Class entity)
     {
-        throw new NotImplementedException();
+        var addClass = await context.Classes.AddAsync(entity);
     }
-
     public async Task UpdateAsync(Class entity)
     {
-        throw new NotImplementedException();
+        var updateClass = context.Classes.Update(entity);
     }
-
     public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
-    }
+        var deleteClass = await context.Classes.FindAsync(id);
 
+        if (deleteClass is null)
+            throw new Exception("No class found with that ID");
+
+        context.Classes.Remove(deleteClass);
+    }
     public async Task<IEnumerable<Class>> GetClassByName(string name)
     {
-        throw new NotImplementedException();
-    }
+        var classByName = await context.Classes.ToListAsync();
 
+        var fuzzyScore = classByName.Select(x => new
+        {
+            Class = x,
+            Score = FuzzySharp.Fuzz.Ratio(name, x.Name)
+        })
+            .Where(c => c.Score > 80)
+            .OrderByDescending(c => c.Score)
+            .Select(c => c.Class);
+
+        if (fuzzyScore is null)
+            throw new Exception("No class found with that name");
+
+        return fuzzyScore;
+    }
     public async Task<IEnumerable<Class>> GetAllPre5EClasses()
     {
-        throw new NotImplementedException();
+        var pre5EClasses = await context.Classes.Where(c => c.IsPre5E == true).ToListAsync();
+
+        if (pre5EClasses is null)
+            throw new Exception("No Pre-5E classes found");
+
+        return pre5EClasses;
     }
 }
